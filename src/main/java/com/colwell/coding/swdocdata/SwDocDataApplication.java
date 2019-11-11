5040a4358007a21e0b1198c59878791de7a26150
unknown <colwell.russell.emerson@gmail.com>
2019-11-11 13:04:46 -0600

    first commit hellow world dockerfile, docker-compose, hello world service

---
+++ src/main/java/com/colwell/coding/swdocdata/SwDocDataApplication.java
@@ -0,0 +1,13 @@
+package com.colwell.coding.swdocdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwDocDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwDocDataApplication.class, args);
+	}
+
+}
